0a73cf772663e0c17e0540264b8b4d58cc7e9f81
nofiiii1 <161667957+nofiiii1@users.noreply.github.com>
2024-03-01 10:40:45 +0700

    Add files via upload

---
+++ flappybrid.nofi/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1709263980,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundbird-sheet0.png",
+		"images/pipa3removebgpreview-sheet0.png",
+		"images/bridping-sheet0.png",
+		"images/pipa3removebgpreview2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
